e04ec1616fb88cd22bc31a1fca78c23f94231e94
kjs <comujs@gmail.com>
2021-07-18 16:38:58 +0900

    이름 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `블로그 시작 테스트`,
     author: {
-      name: `Kyle Mathews`,
+      name: `홍길동`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
