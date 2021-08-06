556e2be5505d8f862838810822927fe6582c7ac1
kjs <comujs@gmail.com>
2021-08-06 14:24:59 +0900

    about style

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,20 +9,22 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import imgA from '../images/sunset.jpg';
+//import imgA from '../../public/brg.jpg';
 
 const AboutPage = ({ location, data }) => (
   <Layout location={location} title={data.site.siteMetadata.title}>
     <Seo title="About" />
     <h1>About Me</h1>
-    <p><img src={ require('brg.jpg') } /></p>
+    <div className="aboutstyle">
+    <p><img alt="iPhone_01" src="/brg.jpg" width='600' height='400'/></p>
     <p>
         안녕하세요<br/>
         다양한 정보를 공유하는 블로그입니다.
     </p>
     <p>급변하는 시대에서 필요한 기술을 접목하여 새로운 가치를 창출할 수 있도록 노력합니다.</p>
     <p>지식을 공유하고 더 나은 방향으로 나아가는 것을 추구합니다.</p>
-    <p><img src={imgA} /></p>
-    <p><img src="brg.jpg" /></p>
+    <p><img src={imgA} alt="iPhone_11" /></p>
+    </div>
   </Layout>
 )
 
