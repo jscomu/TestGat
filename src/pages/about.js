8d2b902adf187b330c66db7336548058a79f12cb
kjs <comujs@gmail.com>
2021-08-06 14:07:00 +0900

    about6

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ const AboutPage = ({ location, data }) => (
   <Layout location={location} title={data.site.siteMetadata.title}>
     <Seo title="About" />
     <h1>About Me</h1>
-    <p><img src={ require('/brg.jpg') } /></p>
+    <p><img src={ require('brg.jpg') } /></p>
     <p>
         안녕하세요<br/>
         다양한 정보를 공유하는 블로그입니다.
@@ -22,6 +22,7 @@ const AboutPage = ({ location, data }) => (
     <p>급변하는 시대에서 필요한 기술을 접목하여 새로운 가치를 창출할 수 있도록 노력합니다.</p>
     <p>지식을 공유하고 더 나은 방향으로 나아가는 것을 추구합니다.</p>
     <p><img src={imgA} /></p>
+    <p><img src="brg.jpg" /></p>
   </Layout>
 )
 
