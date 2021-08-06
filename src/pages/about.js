cbb6486c1642f50889dad81c944237fcbc69f006
kjs <comujs@gmail.com>
2021-08-06 13:36:17 +0900

    about2

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,12 +12,13 @@ const AboutPage = ({ location, data }) => (
   <Layout location={location} title={data.site.siteMetadata.title}>
     <Seo title="About" />
     <h1>About Me</h1>
-    <p><img src='/src/images/cnt.jpg'>사진</img></p>
+    <p></p>
     <p>
         안녕하세요<br/>
         다양한 정보를 공유하는 블로그입니다.
     </p>
-    <p>정보 공유</p>
+    <p>급변하는 시대에서 필요한 기술을 접목하여 새로운 가치를 창출할 수 있도록 노력합니다.</p>
+    <p>지식을 공유하고 더 나은 방향으로 나아가는 것을 추구합니다.</p>
   </Layout>
 )
 
